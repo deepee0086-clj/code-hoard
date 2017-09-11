635bfb59daf18ca8080d5c88a72f6e82e107d8d6
indigo0086 <indigo0086@gmail.com>
2017-09-10 18:51:03 -0700

    Getting closer

--- src/clj/repl_in_your_step.clj
+++ src/clj/repl_in_your_step.clj
@@ -1,5 +1,7 @@
 (ns repl-in-your-step
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.pprint :refer [pprint]]
+            [clojure.string :as str]))
 
 ;;;; Repl-in-your-step
 ;;; This library will provide a context that you can
@@ -10,60 +12,62 @@
 ;;;; Example
 #_(repl-step-do
    500
-   '("Basic Operators"
-     (+ 1 1)
-     (+ 2 2))
+   ("Basic Operators"
+    (+ 1 1)
+    (+ 2 2))
 
-   '("Some Other Stuff"
-     (def a 12)
-     (str "This number is a " a ".")))
+   ("Some Other Stuff"
+    (def a 12)
+    (str "This number is a " a ".")))
 
 ;;;; Output
 ;;; * Basic Operators
+;;;   (ns basic-operators)
 ;;;   (+ 1 1) => 2
 ;;;   (+ 2 2) => 4
 
 ;;; * "Some Other Stuff"
+;;;   (ns some-other-stuf)
 ;;;   (def a 12) =>#'basic-operators/a
 ;;;   (str "This number is a " a ".") => "This number is a 12"
 
-(type ('"testing a b c"))
-(type (symbol "testing a b c"))
 ;;;; Notes
 ;; Will create a namespace for every group of forms, named after the string that appears as the first.
 ;; will automatically iterate through each top level form and print result at provided rate.
 ;; Allow keyboard navigation to step through (stretch).
+(defn make-ns [header]
+  `'(clojure.core/ns `~(-> header (str/replace #" " "_") .toLowerCase)))
+
+(defn print-header [header]
+  `(println ~header))
+
+#_(macroexpand-1 (print-header '("Afterall")))
+
+(defn get-expr-print [expr]
+  `(println '~expr))
+
+#_(macroexpand-1 (get-expr-print '(+ 1 2)))
+
+(defn process-exprs [[header & exprs]]
+  (let [p-head (print-header header)
+        p-exprs (map get-expr-print exprs)]
+    `(~p-head ~p-exprs)))
+
+#_(pprint (macroexpand-1 (process-exprs
+                          '("Afterall"
+                            (+ 1 2)
+                            (+ 3 4)
+                            (/ 1 1)))))
 
 (defmacro repl-step-do
-  [time & forms]
-  (reduce
-   (fn [print-exprs [title & rest-forms]]
-     (let [namespace (.toLowerCase (clojure.string/replace title " " "_"))
-           form-exprs (conj (for [expr rest-forms]
-                              ('print expr))
-                            'do)]
-       (-> print-exprs
-           (conj `(ns ~(symbol namespace)))
-           (conj form-exprs))))
-   `[do]
-   forms))
+  [& forms]
+  `(do
+     ~@(mapcat process-exprs forms)))
 
-(clojure.string/replace "Basic operators" " " "_")
-
-(repl-step-do
- 500
- ("Basic Operators"
-  (+ 1 1)
-  (+ 2 2))
- ("Some Other Stuff"
-  (def a 12)
-  (str "This number is a " a ".")))
-
-(macroexpand `(repl-step-do
-               500
-               `("Basic Operators"
-                 (+ 1 1)
-                 (+ 2 2))
-               `("Some Other Stuff"
-                 (def a 12)
-                 (str "This number is a " a "."))))
+#_(macroexpand-1 (repl-step-do
+                  ("Basic Operators"
+                   (+ 1 1)
+                   (+ 2 2))
+                  #_("Some Other Stuff"
+                     (def a 12)
+                     (str "This number is a " a "."))))
